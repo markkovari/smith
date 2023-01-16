33af2ef5b60e7ee147d6e372bc90ab2de822cdc4
Márk Kővári <kovarimarkofficial@gmail.com>
2023-01-16 19:36:07 +0100

    feat: add option to create config to default home dirs

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,9 @@
+use std::os;
+
+extern crate dirs;
+
 /// Configuration for the application
+#[derive(Debug)]
 pub struct Config {
     /// base folder path
     base_path: String,
@@ -27,5 +32,37 @@ impl Config {
             base_path: base_path.to_string(),
             pattern: pattern.to_string(),
         }
+    }
+
+    /// Create a new Config from a pattern, where the default path is the HOME directory
+    /// # Arguments
+    /// * `pattern` - pattern to match
+    pub fn from_pattern(pattern: &str) -> Self {
+        if pattern.is_empty() {
+            panic!("pattern must not be empty");
+        }
+        Self {
+            base_path: dirs::home_dir()
+                .expect("home directory not found")
+                .to_str()
+                .unwrap()
+                .to_owned(),
+            pattern: pattern.to_string(),
+        }
+    }
+
+    pub fn from_extended_base_path_and_pattern(extended_path: &str, pattern: &str) -> Self {
+        if extended_path.is_empty() || pattern.is_empty() {
+            panic!("base_path and pattern must not be empty");
+        }
+        let default_home = dirs::home_dir()
+            .expect("home directory not found")
+            .to_str()
+            .unwrap()
+            .to_owned();
+        let mut default_path = std::path::Path::new(&default_home);
+        let binding = default_path.join(extended_path);
+        default_path = binding.as_path();
+        Self::new(default_path.to_str().unwrap(), pattern)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ mod config;
 
 /// Main function
 fn main() {
-    let config = Config::new("path/to/base/folder", "pattern");
+    let config = Config::new("/Users/markkovari/DEV", "pattern");
+    let pattern_only = Config::from_extended_base_path_and_pattern("DEV", "pattern");
+    println!("{:?}", config);
+    println!("{:?}", pattern_only);
     println!("Hello, Shoot me in the head!");
 }
