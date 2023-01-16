e2010eef97d62c3aa166775437eef35351c9c383
Márk Kővári <kovarimarkofficial@gmail.com>
2023-01-16 19:43:34 +0100

    feat: change patterns to string vector

--- src/config.rs
+++ src/config.rs
@@ -8,7 +8,7 @@ pub struct Config {
     /// base folder path
     base_path: String,
     /// pattern to match
-    pattern: String,
+    patterns: Vec<String>,
 }
 
 impl Config {
@@ -24,21 +24,21 @@ impl Config {
     /// let config = Config::new("path/to/base/folder", "pattern");
     /// ```
     /// panics if `base_path` or `pattern` is empty
-    pub fn new(base_path: &str, pattern: &str) -> Self {
-        if base_path.is_empty() || pattern.is_empty() {
+    pub fn new(base_path: &str, patterns: Vec<String>) -> Self {
+        if base_path.is_empty() || patterns.is_empty() {
             panic!("base_path and pattern must not be empty");
         }
         Self {
             base_path: base_path.to_string(),
-            pattern: pattern.to_string(),
+            patterns,
         }
     }
 
     /// Create a new Config from a pattern, where the default path is the HOME directory
     /// # Arguments
     /// * `pattern` - pattern to match
-    pub fn from_pattern(pattern: &str) -> Self {
-        if pattern.is_empty() {
+    pub fn from_pattern(patterns: Vec<String>) -> Self {
+        if patterns.is_empty() {
             panic!("pattern must not be empty");
         }
         Self {
@@ -47,12 +47,12 @@ impl Config {
                 .to_str()
                 .unwrap()
                 .to_owned(),
-            pattern: pattern.to_string(),
+            patterns,
         }
     }
 
-    pub fn from_extended_base_path_and_pattern(extended_path: &str, pattern: &str) -> Self {
-        if extended_path.is_empty() || pattern.is_empty() {
+    pub fn from_extended_base_path_and_pattern(extended_path: &str, patterns: Vec<String>) -> Self {
+        if extended_path.is_empty() || patterns.is_empty() {
             panic!("base_path and pattern must not be empty");
         }
         let default_home = dirs::home_dir()
@@ -63,6 +63,6 @@ impl Config {
         let mut default_path = std::path::Path::new(&default_home);
         let binding = default_path.join(extended_path);
         default_path = binding.as_path();
-        Self::new(default_path.to_str().unwrap(), pattern)
+        Self::new(default_path.to_str().unwrap(), patterns)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,9 @@ mod config;
 
 /// Main function
 fn main() {
-    let config = Config::new("/Users/markkovari/DEV", "pattern");
-    let pattern_only = Config::from_extended_base_path_and_pattern("DEV", "pattern");
+    let config = Config::new("/Users/markkovari/DEV", vec!["pattern".to_owned()]);
+    let pattern_only =
+        Config::from_extended_base_path_and_pattern("DEV", vec!["pattern".to_owned()]);
     println!("{:?}", config);
     println!("{:?}", pattern_only);
     println!("Hello, Shoot me in the head!");
